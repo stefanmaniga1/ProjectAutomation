838db5af234fdf80c780997bf35b13f45039b33f
BojteTamas <bojtetomi92@gmail.com>
2019-10-22 13:31:56 +0300

    Am adaugat ceva frumos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Branch 1");
-        System.out.println("Added the second branch ERROR");
+        System.out.println("Ceva frumos");
     }
 }
